0fb9b1ab1b041622ccb97229d71bad88bf5d38b3
LeeTaeHoon97 <59239082+LeeTaeHoon97@users.noreply.github.com>
2022-05-18 00:40:15 +0900

    update

---
+++ model.py
@@ -0,0 +1,24 @@
+from keras.models import load_model
+from PIL import Image, ImageOps
+import numpy as np
+
+class rpsNet:
+    model=None
+    def __init__():
+        #load model
+        self.model=load_model('keras_model.h5')
+    def predict(data):
+        return self.model.predict(data)
+        
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+image = Image.open('<IMAGE_PATH>')
+size = (224, 224)
+image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+#turn the image into a numpy array
+image_array = np.asarray(image)
+# Normalize the image
+normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+# Load the image into the array
+data[0] = normalized_image_array
+
